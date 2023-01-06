ef064a46e103d47d7ba31dfcee7e066a0c96c0e0
addis <betsega23@gmail.com>
2023-01-06 21:42:46 +0300

    Fix linters

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ReservationsController < ApplicationController
-  before_action :set_reservation, only: %i[ show edit update destroy ]
+  before_action :set_reservation, only: %i[show edit update destroy]
 
   # GET /reservations or /reservations.json
   def index
@@ -8,8 +8,7 @@ class Api::V1::ReservationsController < ApplicationController
   end
 
   # GET /reservations/1 or /reservations/1.json
-  def show
-  end
+  def show; end
 
   # GET /reservations/new
   def new
@@ -17,8 +16,7 @@ class Api::V1::ReservationsController < ApplicationController
   end
 
   # GET /reservations/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /reservations or /reservations.json
   def create
@@ -26,7 +24,7 @@ class Api::V1::ReservationsController < ApplicationController
 
     respond_to do |format|
       if @reservation.save
-        format.html { redirect_to reservation_url(@reservation), notice: "Reservation was successfully created." }
+        format.html { redirect_to reservation_url(@reservation), notice: 'Reservation was successfully created.' }
         format.json { render :show, status: :created, location: @reservation }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +37,7 @@ class Api::V1::ReservationsController < ApplicationController
   def update
     respond_to do |format|
       if @reservation.update(reservation_params)
-        format.html { redirect_to reservation_url(@reservation), notice: "Reservation was successfully updated." }
+        format.html { redirect_to reservation_url(@reservation), notice: 'Reservation was successfully updated.' }
         format.json { render :show, status: :ok, location: @reservation }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,19 +51,20 @@ class Api::V1::ReservationsController < ApplicationController
     @reservation.destroy
 
     respond_to do |format|
-      format.html { redirect_to reservations_url, notice: "Reservation was successfully destroyed." }
+      format.html { redirect_to reservations_url, notice: 'Reservation was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reservation
-      @reservation = Reservation.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def reservation_params
-      params.require(:reservation).permit(:startDate, :endDate)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def reservation_params
+    params.require(:reservation).permit(:startDate, :endDate)
+  end
 end
--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ToursController < ApplicationController
-  before_action :set_tour, only: %i[ show edit update destroy ]
+  before_action :set_tour, only: %i[show edit update destroy]
 
   # GET /tours or /tours.json
   def index
@@ -8,8 +8,7 @@ class Api::V1::ToursController < ApplicationController
   end
 
   # GET /tours/1 or /tours/1.json
-  def show
-  end
+  def show; end
 
   # GET /tours/new
   def new
@@ -17,8 +16,7 @@ class Api::V1::ToursController < ApplicationController
   end
 
   # GET /tours/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /tours or /tours.json
   def create
@@ -26,7 +24,7 @@ class Api::V1::ToursController < ApplicationController
 
     respond_to do |format|
       if @tour.save
-        format.html { redirect_to tour_url(@tour), notice: "Tour was successfully created." }
+        format.html { redirect_to tour_url(@tour), notice: 'Tour was successfully created.' }
         format.json { render :show, status: :created, location: @tour }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +37,7 @@ class Api::V1::ToursController < ApplicationController
   def update
     respond_to do |format|
       if @tour.update(tour_params)
-        format.html { redirect_to tour_url(@tour), notice: "Tour was successfully updated." }
+        format.html { redirect_to tour_url(@tour), notice: 'Tour was successfully updated.' }
         format.json { render :show, status: :ok, location: @tour }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,19 +51,20 @@ class Api::V1::ToursController < ApplicationController
     @tour.destroy
 
     respond_to do |format|
-      format.html { redirect_to tours_url, notice: "Tour was successfully destroyed." }
+      format.html { redirect_to tours_url, notice: 'Tour was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tour
-      @tour = Tour.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def tour_params
-      params.require(:tour).permit(:tourName, :picture, :description, :hotel, :price)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_tour
+    @tour = Tour.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def tour_params
+    params.require(:tour).permit(:tourName, :picture, :description, :hotel, :price)
+  end
 end
