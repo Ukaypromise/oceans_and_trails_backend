71f9a4c67c2a67519aa19200f906559b8dfc84b6
addis <betsega23@gmail.com>
2023-01-09 23:23:02 +0300

    Add JWT authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Api
+class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
