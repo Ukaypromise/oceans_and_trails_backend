f724d441d8a2c274c19e06edcade8e448b56117f
Federico Luna <fluna1973@yahoo.com>
2023-01-10 13:28:46 -0600

    add documentations available with API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # devise_for :users, controllers: { sessions: 'api/v1/users/sessions', 
   #                                   registrations: 'api/v1/users/registrations'}
-
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
 
    namespace :api do
     namespace :v1 do
