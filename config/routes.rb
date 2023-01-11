1b1e97625b76b5ff6cdfb57a8cbec5aad62e61ff
Federico Luna <fluna1973@yahoo.com>
2023-01-11 08:40:52 -0600

    add route to register

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 
    namespace :api do
     namespace :v1 do
-      devise_for :users, controllers: { sessions: 'api/v1/users/sessions',
+      devise_for :users, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registrations: 'signup'
+        }, controllers: { sessions: 'api/v1/users/sessions',
          registrations: 'api/v1/users/registrations' }  
       end
     end
