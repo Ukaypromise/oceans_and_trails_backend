ac4b38c0367e43cb302b5fb6a58bc9b58ecf673c
Federico Luna <fluna1973@yahoo.com>
2023-01-11 11:04:42 -0600

    modified sessions controllers

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -3,28 +3,28 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
 
   respond_to :json
 
-  # private
+  private
 
-  # def respond_with(resource, _opts = {})
-  #   render json: {
-  #   status: { code: 200, message: 'Logged in sucessfully.' },
-  #   data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-  #   }, status: :ok
-  #   end
+  def respond_with(resource, _opts = {})
+    render json: {
+    status: { code: 200, message: 'Logged in sucessfully.' },
+    data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+    }, status: :ok
+    end
 
-  #   def respond_to_on_destroy
-  #   if current_user
-  #   render json: {
-  #   status: 200,
-  #   message: 'logged out successfully'
-  #   }, status: :ok
-  #   else
-  #   render json: {
-  #   status: 401,
-  #   message: "Couldn't find an active session."
-  #   }, status: :unauthorized
-  #   end
-  #   end
+    def respond_to_on_destroy
+    if current_user
+    render json: {
+    status: 200,
+    message: 'logged out successfully'
+    }, status: :ok
+    else
+    render json: {
+    status: 401,
+    message: "Couldn't find an active session."
+    }, status: :unauthorized
+    end
+    end
 
   # GET /resource/sign_in
 
@@ -35,7 +35,7 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  end
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
