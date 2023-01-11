428ef8864eb10e87419b76b37fd123a1c74e055c
Federico Luna <fluna1973@yahoo.com>
2023-01-11 10:34:45 -0600

    add sign up route and user

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       devise_for :users, path: '', path_names: {
         sign_in: 'login',
         sign_out: 'logout',
-        registrations: 'signup'
+        sign_up: 'signup'
         }, controllers: { sessions: 'api/v1/users/sessions',
          registrations: 'api/v1/users/registrations' }  
       end
