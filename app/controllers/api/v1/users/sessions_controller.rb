054f195019d4f0c401be606bc11a7f2328c83261
Federico Luna <fluna1973@yahoo.com>
2023-01-12 11:20:19 -0600

    change logout params

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -10,16 +10,17 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    current_user ? log_out_success : log_out_failure
-  end
-
-  def log_out_success
     render json: { message: 'Logged out.' }, status: :ok
+    # current_user ? log_out_success : log_out_failure
   end
 
-  def log_out_failure
-    render json: { message: 'Logged out failure.'}, status: :unauthorized
-  end
+  # def log_out_success
+  #   render json: { message: 'Logged out.' }, status: :ok
+  # end
+
+  # def log_out_failure
+  #   render json: { message: 'Logged out failure.'}, status: :unauthorized
+  # end
 
   # private
 
