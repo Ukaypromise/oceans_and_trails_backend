42e0a3eddaaab3811ed7bfc2eac4323e273fac01
addis <betsega23@gmail.com>
2023-01-12 20:28:36 +0300

    Add validation for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   has_many :tours, through: :reservations
 
   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
